1f1f5b02c0e07eff44741d8b90d1f1313f17ed4c
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-01 23:40:58 -0400

    deceeee 🌙

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const { scene, camera, renderer } = createScene({
 })
 window.scene = scene
 
-
 camera.position.set(0,4,2)
 const cLight = new THREE.PointLight(0xffffff, 1, 1000)
 camera.add(cLight)
@@ -20,18 +19,91 @@ scene.add(camera)
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
 
 import sceneGraphConstructor from './scene'
-const sceneGraph = sceneGraphConstructor()
 
-const keyframes = []
-for (let obj3DKey of Object.keys(sceneGraph)) {
-  const obj3D = sceneGraph[obj3DKey]
+const meshes = sceneGraphConstructor()
 
-  if (obj3D.keyframe) {
-    keyframes.push(obj3D.keyframe)
+console.log(meshes)
+
+
+// const keyframes = []
+// for (let obj3DKey of Object.keys(meshes)) {
+//   const obj3D = meshes[obj3DKey]
+
+//   if (obj3D.keyframe) {
+//     keyframes.push(obj3D.keyframe)
+//   }
+
+//   scene.add(obj3D)
+// }
+
+const cube2 = meshes.cube.clone()
+
+// const positions = [
+//   [-5, 5],
+//   [-4, -4]
+// ]
+
+const position = ({ x, y, z }) => new THREE.Vector3(x, y, z)
+// const xyzBounds =
+
+// const position = ({ minX = 0, minY = 0, minZ = 0 },  { maxX = 0, maxY = 0, maxZ = 0 }) => {
+//   return [
+//     new THREE.Vec
+//   ]
+// }
+
+const positions = []
+
+const minX = -5
+const minZ = -5
+const maxX = 5
+const maxZ = 5
+const minY = -5
+const maxY = 5
+
+const positionsXZ = []
+
+const XZy = 2
+
+for (let x = minX; x < maxX; x++) {
+  if (positionsXZ[x] === undefined) {
+    positionsXZ[x] = []
   }
 
-  scene.add(obj3D)
+  for (let z = minZ; z < maxZ; z++) {
+    positionsXZ[x][z] = new THREE.Vector3(x, z, XZy)
+  }
 }
+
+import { flatUIHexColors } from './lib/colour-utils.js'
+
+for (let x = minX; x < maxX; x++) {
+  for (let z = minZ; z < maxZ; z++) {
+    for (let y = minY; y < maxY; y++) {
+
+      const material = new THREE.MeshLambertMaterial({ color: flatUIHexColors[Math.floor(Math.random() * flatUIHexColors.length)] })
+      material.transparent = true
+      material.opacity = 0.5
+
+      const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        // new THREE.MeshLambertMaterial({ color: 0x0E50F2 })
+        material
+      )
+      // cube.position.set(positionsXZ[x].x, positionsXZ[y].y, positionsXZ[z])
+      cube.position.set(x, y, z)
+
+      scene.add(cube)
+
+    }
+  }
+}
+
+
+cube2.position.x = -3
+cube2.position.z = -2
+
+scene.add(cube2)
 
 window.capturer = new CCapture( { format: 'png' } )
 // capturer.start()
@@ -42,9 +114,9 @@ const stats = createStats()
 const render = () => {
   stats.begin()
 
-  for (let keyframe of keyframes) {
-    keyframe()
-  }
+  // for (let keyframe of keyframes) {
+  //   keyframe()
+  // }
 
   renderer.render(scene, camera)
 
